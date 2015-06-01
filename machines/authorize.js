22661e9e5a830bf4040b0e7e324ae2785a4e2455
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2015-06-01 11:08:40 -0700

    init

---
+++ machines/authorize.js
@@ -0,0 +1,41 @@
+module.exports = {
+
+
+  friendlyName: 'Authorize',
+
+
+  description: 'Get authorization tokens',
+
+
+  cacheable: false,
+
+
+  sync: false,
+
+
+  idempotent: false,
+
+
+  inputs: {
+
+  },
+
+
+  exits: {
+
+    success: {
+      variableName: 'result',
+      description: 'Done.',
+    },
+
+  },
+
+
+  fn: function (inputs,exits
+  /**/) {
+    return exits.success();
+  },
+
+
+
+};
